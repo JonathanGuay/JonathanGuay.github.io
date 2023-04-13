15c4ed7431e7cfa053a64251c0a07850411891a1
JonathanGuay <116024586+JonathanGuay@users.noreply.github.com>
2023-04-13 11:28:32 -0400

    changement de mon projet

--- scriptModifier.js
+++ scriptModifier.js
@@ -1,9 +1,19 @@
-function Chanson(nom="", artiste="", image=""){
-    this.nom = nom;
-    this.artiste = artiste;
-    this.image = image;
+// propriété et constructeur de la classe chanson
+class Chanson{
+    constructor(nom="", artiste="", image="") {
+        this.nom = nom;
+        this.artiste = artiste;
+        this.image = image;
+    }
 }
 
+// Affiche le message informatif quand on entre une lettre dans le nom
+$("#name").keyup(function(){
+    $(".text-danger-emphasis").removeClass("invisible");
+});
+
+
+// affiche les chansons comme je veux
 function afficheChanson(chanson) {
     $.getJSON('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson')
         .done(function (chansons) {
@@ -11,101 +21,88 @@ function afficheChanson(chanson) {
                 $('#ajoutDeChanson').append(`
                     <div class="carousel-item q">
                     <img class="d-block w-100" src="${chanson.image}" alt="">
-                    <p class="mt-3" id='chanson${chanson.id}'>ID : ${chanson.id}</p>
+                    <p class="mt-3" id="id">ID : ${chanson.id}</p>
                     <p>Titre : ${chanson.nom}</p>
                     <p class="mb-2">Artiste : ${chanson.artiste}</p>
-                    <button class="btn btn-primary" value="Supprimer" onclick="supprimer()">Supprimer</button>
                     </div>`)
-        }
-    });
+            }
+        });
 }
-// Bouton ajouter
-$("form").submit(function (){
-    $.ajax('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson', {
-        data : JSON.stringify({"nom": $("#name").val(), "artiste": $("#artiste").val(), "image": $("#img").val() }),
-        contentType : 'application/json',
-        type : 'POST'
-    });
-});
 
+// bouton ajouter fais aussi envoie du formulaire
+function ajouter(){
+    let numID = $("#numID").val();
+    // créer un objet chanson
+    const chanson = new Chanson(
+        $('#name').val(),
+        $('#artiste').val(),
+        $('#img').val()
+    );
+    $.ajax('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson', {
+        data: JSON.stringify(chanson),
+        contentType: 'application/json',
+        type: 'POST',
+         })
+    // enregistre l'id dans le local storage, en concatènant le nom du local storage
+    localStorage.setItem(`musique${numID}`, numID.toString());
+}
+
+// bouton modifier
 function modifier(){
-    fetch('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson/'+$("#id").val(),{
+    let numID = $("#numID").val();
+    // créer un nouveau objet qui va remplacer l'autre
+    const chanson = new Chanson(
+        $('#name').val(),
+        $('#artiste').val(),
+        $('#img').val()
+    );
+    // va chercher id dans local storage pour voir si l'utilisateur à ajouter la chanson
+    const chansonStorage = JSON.parse(localStorage.getItem(`musique${numID}`));
+    // compare id trouver avec celui du local storage
+    if (chansonStorage.toString() === numID){
+    fetch('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson/'+numID,{
         method: 'PUT',
         headers: {'content-type':'application/json'},
-        body : JSON.stringify({"nom": $("#name").val(), "artiste" : $("#artiste").val(), "image": $("#img").val()})
+        body : JSON.stringify(chanson)
     }).then(function (){
-        $("id"+$("#id").val()).text($("#id").val() + ", " + $("#name").val() + ", " + $("#artiste").val() + ", " + $("#img").val());
+        $("#chanson"+$("#id").val()).text($("#id").val() + ", " + $("#name").val() + ", " + $("#artiste").val() + ", " + $("#img").val());
     })
+    // si l'id est pas trouvé l'utilisateur ne peut pas modifier la chanson
+    } else{
+        alert("Vous ne pouvez pas modifier cette chanson");
+    }
 }
 
-// pour le lien URL mettre le lien / mettre le nom de la resource
+// bouton supprimer
 function supprimer(){
-    fetch('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson/' +$("#id").val(), {
-        method: 'DELETE',
-    }).then(function (){
-        $("id"+$("#id").val()).remove();
-    });
+    let numID = $("#numID").val();
+    // enlève le champ required des inputs quand on supprime parce qu'on a seulement besoin l'id
+    $("#name").removeAttr("required");
+    $("#artiste").removeAttr("required");
+    $("#img").removeAttr("required");
+    // va chercher id dans local storage pour voir si l'utilisateur à ajouter la chanson
+    const chansonStorage = JSON.parse(localStorage.getItem(`musique${numID}`));
+    // compare id trouver avec celui du local storage
+    if (chansonStorage.toString() === numID) {
+                fetch('https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson/' + numID, {
+                    method: 'DELETE',
+                }).then(function () {
+                    $("#chanson" + $("#id").val()).remove()
+                        .catch(error => console.error(error));
+                });
+    // si l'id est pas trouvé l'utilisateur ne peut pas supprimer la chanson
+    } else {
+        alert("Vous ne pouvez pas supprimer cette chanson");
+    }
 }
+
 afficheChanson();
 
 
 
-
-
-
-
-
-// pour enregistrement en local
-/*
-
-function stockage2(){
-    // Créer un objet
-    const name = $('#name').val();
-    const artiste = $('#artiste').val();
-    const image = $('#image').val();
-
-    const chanson = new Chanson(name, artiste, image);
-
-    // Stocker l'objet dans le stockage local
-    localStorage.setItem('LesChansons', JSON.stringify(chanson))
-    return true;
-}
-
-const affiche = JSON.parse(localStorage.getItem('LesChansons'));
-
-let liste = JSON.parse(localStorage.getItem('ListeDeChansons'));
-
-if(liste === null){
-    liste = [] // si c'est la première fois la liste est null donc il faut l'
-}
-
-liste.push(affiche);
-localStorage.setItem('ListeDeChansons', JSON.stringify(liste));
-
-for(i = 0; i < liste.length; i++){
-    afficheChanson(liste[i]);
-}
-
-*/
-
-
-// pour enregistrement sur serveur mockapi
-/*
-
-
-
-*/
-
-
-
-
-
-
-// Lien serveur Mockapi :
-// https://64248bbe9e0a30d92b1e6948.mockapi.io/Chanson
-
-
-
+//$("#btnSupprimer").on("click", function() {
+   // if (confirm("Êtes-vous sûr de vouloir supprimer cette chanson ?")) {
+   // }
 
 
 
